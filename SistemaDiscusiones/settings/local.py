f72b507a5ff9ec0764e5d05bdc5f618890775c35
Elicia <elicia_cor@hotmail.com>
2014-04-04 15:37:09 -0500

    comenzando

---
+++ SistemaDiscusiones/settings/local.py
@@ -0,0 +1,39 @@
+from .base import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'discusiones.db',
+		# 'USER' : 'cursodjango',
+		# 'PASSWORD' : 'pass',
+		# 'HOST' : 'localhost',
+		# 'PORT' : '5432',
+	}
+}
+ 
+STATIC_URL = '/static/'
+ 
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1422409964680771'
+SOCIAL_AUTH_FACEBOOK_SECRET = '640df1ac8fcb8da6dbc59209f3635069'
+
+SOCIAL_AUTH_TWITTER_KEY = 'qWHWMSKEnshW2gukH5MQ'
+SOCIAL_AUTH_TWITTER_SECRET = 'jviFtewE4lDhXIR54M61Ni8tyDlrVPpRbTjOj4NRVqU'
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+STATIC_ROOT = 'staticfiles'
